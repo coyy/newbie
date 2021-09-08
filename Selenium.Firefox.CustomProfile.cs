d1e08039642520b7ae398e9f82f4663bea65cb11
coyy <41223888+coyy@users.noreply.github.com>
2021-09-09 00:02:06 +0700

    Update Selenium.Firefox.CustomProfile.cs

--- Selenium.Firefox.CustomProfile.cs
+++ Selenium.Firefox.CustomProfile.cs
@@ -1,7 +1,14 @@
 string profileLocal = @"C:\test1";
+int portWebdriver = new Random().Next(20000, 30000);
+List<string> newProfile = new List<string>();
+newProfile.Add("user_pref(\"marionette.contentListener\", true);");
+newProfile.Add("user_pref(\"marionette.enabled\", true);");
+newProfile.Add("user_pref(\"marionette.port\", " + portWebdriver + ");");
+File.WriteAllLines(profileLocal + "\\prefs.js", newProfile);
 FirefoxDriverService firefoxService = FirefoxDriverService.CreateDefaultService();
 FirefoxOptions optionsFirefox = new FirefoxOptions();
 firefoxService.HideCommandPromptWindow = true;
+firefoxService.BrowserCommunicationPort = portWebdriver;
 optionsFirefox.AddArgument("-profile");
 optionsFirefox.AddArgument(profileLocal);
 var webDriver = new FirefoxDriver(firefoxService, optionsFirefox);
